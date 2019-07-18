67976d8472f3da6bb779a2b6023d41b70568a6b4
Alex Devonport <alex_devonport@berkeley.edu>
2019-07-18 13:34:51 +0200

    add speed tests

---
+++ run_speed_test_mc.sh
@@ -0,0 +1,15 @@
+cd ../../bin
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_10/ex_n_link.cfg -d 1 -p | tee ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_20/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_50/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_100/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_200/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_500/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_1000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_2000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_5000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_10000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_20000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_50000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_mc/ex_n_link_100000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_mc/results.txt
+cd ../examples/pirk
---
+++ speed_test_mc/ex_n_link_10/growth_bound_matrix.cl
@@ -0,0 +1,34 @@
+float growth_bound_matrix(int i, int j, __global float* u)
+{
+    // Parameters
+    int nlinks = 10;
+    float v = 0.5;            // free-flow speed, in links/period
+    float w =  1.0/6.0;            // congestion-wave speed, in links/period
+    float xbar = 320.0;         // max occupancy when jammed, in vehicles
+    float b =  3.0/4.0;            // fraction of vehicule staying on the network after each link
+    float T = 30.0;             // time step for the continuous-time model
+    float c = 0.0;
+
+    if (i==0 && j == 1) {
+        c = 2 * w;
+    } else if (i == 0 && j == 2) {
+        c = 2 * w;
+    } else if (i == 1 && j == 0) {
+        c = 0.5 * v;
+    } else if (i == 2 && j == 0) {
+        c = 0.5 * v;
+    } else if (i - j == 2) {
+        c = b * v;
+    } else if (i - j == -2) {
+        c = w / b;
+    } else if (i == (nlinks - 1) && j == (nlinks - 1) - 2) {
+        c = b * v;
+    } else if (i == (nlinks - 1) - 1 && j == (nlinks - 1) - 3) {
+        c = b * b;
+    }
+    else {
+        c = 0;
+    }
+    c = c / T;
+    return c;
+}
