a84926ff4c7bccd8a66d6502ce9bc8b914b2a3b3
Alex Devonport <alex_devonport@berkeley.edu>
2019-07-19 10:04:44 +0200

    remove if statement from sparsification kernel

--- kernel-pack/growth_bound/gb_compute_contraction_matrix.cl
+++ kernel-pack/growth_bound/gb_compute_contraction_matrix.cl
@@ -5,22 +5,6 @@ __kernel void gb_compute_contraction_matrix(__global int *cidxs, __global float 
     float c;
     for (int col_idx = 0; col_idx < @@states_dim@@; col_idx++) {
        c = growth_bound_matrix(row_idx, col_idx);
-       //if(row_idx==0) {printf("%d %f\n",col_idx, c);}
-       if(c != 0.) {
-           if(row_idx==0) printf("gonna save the index %d to position %d, with the value %f\n",col_idx, nonzero_count, c);
-           cidxs[row_idx * @@row_max@@ + nonzero_count] = col_idx;
-           cvals[row_idx * @@row_max@@ + nonzero_count] = c;
-           nonzero_count++;
-       }
     }
     ncel[row_idx] = nonzero_count;
-    //if(row_idx == 0){
-    //    printf("nonzero elements in row %d of contraction matrix: %d\n", row_idx, ncel[row_idx]);
-    //    printf("elements in row %d:\n",row_idx);
-    //    int idx;
-    //    for(int q = 0; q < ncel[row_idx];q++){
-    //        idx = cidxs[row_idx*@@row_max@@ + q];
-    //        printf("[%d], col %d: %f\n",q, idx, cvals[row_idx*@@row_max@@ + q]);
-     //   }
-    //}
 }
--- run_speed_test_gb.sh
+++ run_speed_test_gb.sh
@@ -1,27 +1,27 @@
 cd ../../bin
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_10/ex_n_link.cfg -d 1 -p | tee ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_20/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_50/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_100/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_200/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_500/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_1000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_2000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_5000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_10000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_20000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_50000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_100000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_200000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_500000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_1000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_2000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_5000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_10000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_20000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_50000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_100000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_200000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_500000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
-./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_1000000000/ex_n_link.cfg -d 1 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_10/ex_n_link.cfg -d 2 -p | tee ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_20/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_50/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_100/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_200/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_500/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_1000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_2000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_5000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_10000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_20000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_50000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_100000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_200000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_500000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_1000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_2000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_5000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_10000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_20000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_50000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_100000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_200000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_500000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
+./pfaces -CGH -k pirk.cpu@../examples/pirk/kernel-pack/ -cfg ../examples/pirk/speed_test_gb/ex_n_link_1000000000/ex_n_link.cfg -d 2 -p | tee -a ../examples/pirk/speed_test_gb/results.txt
 cd ../examples/pirk
