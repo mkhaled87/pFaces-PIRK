34098f3ce1750a5207b1879349477baec4eb19de
M. Khaled (MBP) <eng.mk@msn.com>
2020-12-29 09:59:03 +0100

    adding CMake as an easier build system for Windows, Mac and Linux

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# CMake cofiguration
+BUILDTYPE=Release
+KERNEL_NAME=pirk
+
+# remove old build files and binaries
+rm -rf kernel-pack/$KERNEL_NAME
+
+# building ...
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=$BUILDTYPE
+cmake --build . --config $BUILDTYPE
+cd ..
+rm -rf build
